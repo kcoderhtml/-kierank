7e841f9cae2c16131d89abc5c062a03955033002
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-01-27 20:59:25 -0500

    feat: add simple gen

--- get-art.sh
+++ get-art.sh
@@ -14,13 +14,26 @@ TEXT="Hello! Welcome to my site"
 ENCODED_TEXT=$(url_encode "$TEXT")
 
 # Get the list of available fonts
-fonts=$(curl -s "$FONTS_ENDPOINT" | jq -r '.fonts[]')
+# fonts=$(curl -s "$FONTS_ENDPOINT" | jq -r '.fonts[]')
+fonts=("1Row" "3-D" "3D Diagonal" "3D-ASCII")
+
+# Create an array to store results
+results=()
 
 # Iterate over each font and fetch the ASCII art
-for font in $fonts; do
+for font in "${fonts[@]}"; do
     echo "Fetching ASCII art: $API_ENDPOINT?text=$ENCODED_TEXT&font=$font"
-    result=$(curl -s "$API_ENDPOINT?text=$ENCODED_TEXT&font=$font")
-    ascii_art=$(echo "$result")
+    result=$(curl -s "$API_ENDPOINT?text=$ENCODED_TEXT&font=$font" | sed ':a;N;$!ba;s/\n/\\n/g')
     
-    echo -e "\nFont: $font\n$ascii_art\n"
+    # Add result to the array
+    results+=("{\"font\": \"$font\", \"ascii_art\": \"$result\"}")
 done
+
+# Convert the array to JSON
+json_output=$(printf '{"results": [%s]}' "$(IFS=,; echo "${results[*]}")")
+
+# Save the JSON to a file
+output_file="ascii_results.json"
+echo "$json_output" > "$output_file"
+
+echo "Results saved to $output_file"
