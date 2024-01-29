7494f8774cce6d85efdc7d22ff6132643f1e87ec
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-01-28 20:58:26 -0500

    feat: removed fonts that were too large

--- get-art.sh
+++ get-art.sh
@@ -7,7 +7,7 @@ TEXT="Hello this is Lorem Ipsum"
 LIMIT=0
 UNLIMITED=true
 # FONTS=() # leave empty to use the API to get the list of fonts
-FONTS=("Fraktur" "Fuzzy" "Georgi16" "Georgia11" "Ghost" "Gothic" "Graceful" "Graffiti" "Henry 3D" "Hex" "ICL-1900" "Impossible" "Isometric1" "Isometric2" "Isometric3" "Isometric4" "JS Bracket Letters" "JS Stick Letters" "Keyboard" "Larry 3D 2" "Lean" "Letters" "Marquee")
+FONTS=("Fuzzy" "Georgi16" "Georgia11" "Ghost" "Gothic" "Graceful" "Graffiti" "Hex" "ICL-1900" "Keyboard" "Lean" "Letters" "Marquee")
 
 # get parameters
 while getopts ":t:l:f" opt; do
